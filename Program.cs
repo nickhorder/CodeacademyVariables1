06ee57938556b21e08796e46cf1c94aa85bc115c
nickhorder <thunderstruck1986@hotmail.co.uk>
2021-05-08 20:23:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codeacademy_C__Variables1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Variables
+            string name = "Shadow";
+            string breed = "Golden Retriever";
+            int age = 5;
+            double weight = 65.22;
+            bool spayed = true;
+            // Print variables to the console
+            Console.WriteLine(name);
+            //Console.Write(", who is a ");
+            Console.WriteLine(breed);
+            //Console.Write(", is the age of ");
+            Console.WriteLine(age);
+            //Console.Write(". the dog weighs ");
+            Console.WriteLine(weight);
+            Console.WriteLine(spayed);
+        }
+    }
+}
